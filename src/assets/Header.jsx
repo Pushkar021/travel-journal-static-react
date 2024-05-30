f5d4f4d97e764fcff8b5d883721e7563e29e7ba8
Pushkar021 <fantumdeluxe15@gmail.com>
2024-05-30 15:09:03 +0530

    fix img problem

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header(){
     return (
       <nav className="header">
-        <img src="../public/images/worldlogo.png" />
+        <img src="./public/images/worldlogo.png" />
         <p>My Travel Journal</p>
       </nav>
     );
--- src/assets/card.jsx
+++ src/assets/card.jsx
@@ -5,12 +5,12 @@ export default function Card(props){
     return (
       <div className="main">
         <div className="main1">
-          <img src={`../images/${props.img}`} />
+          <img src={`./images/${props.img}`} />
         </div>
         <div className="width">
           <div>
             <div className="main2">
-              <img src="../public/images/pointer.png" />
+              <img src="./public/images/pointer.png" />
               <p className="country">{props.countryname}</p>
               <a href="https://www.google.com" target="_blank">
                 View on Google maps
